4cedf1da2a28b49a3bc09ac3afc9ffdb7a8af45f
Andrii Dubniuk <brufer422@gmail.com>
2019-07-07 19:21:44 +0300

    Initial commit

---
+++ ip/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace ip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "KamikoBot";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(100, 48);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(99, 22);
+            this.showToolStripMenuItem.Text = "Chat";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(99, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(12, 373);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Made by Charisma";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(316, 35);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(226, 301);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(467, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(42, 35);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(226, 301);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(559, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(22, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Gold;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(531, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(22, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "_";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.CancelButton = this.button2;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(583, 402);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ip";
+            this.TopMost = true;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.VisibleChanged += new System.EventHandler(this.Form1_VisibleChanged);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ip/Form1.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using SimpleJSON;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Collections.Specialized;
+
+namespace ip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        WebClient webClient = new WebClient();
+        DateTime StartSession;
+        string Token = "541751696:AAHp-XMrbGqnuTdad1Hw4WbhnePDaWKI04s";
+        bool flag = false;
+        private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+        private const int APPCOMMAND_VOLUME_UP = 0xA0000;
+        private const int APPCOMMAND_VOLUME_DOWN = 0x90000;
+        private const int WM_APPCOMMAND = 0x319;
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg,
+            IntPtr wParam, IntPtr lParam);
+
+        private void Mute()
+        {
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+                (IntPtr)APPCOMMAND_VOLUME_MUTE);
+        }
+
+        private void VolDown()
+        {
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+                (IntPtr)APPCOMMAND_VOLUME_DOWN);
+        }
+
+        private void VolUp()
+        {
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+                (IntPtr)APPCOMMAND_VOLUME_UP);
+        }
+
+        public void SendMessage(string Message)
+        {
+            webClient.DownloadString("https://api.telegram.org/bot" + Token + "/sendMessage?chat_id=349870760&text=" + Message);
+        }
+
+        //https://api.telegram.org/bot541751696:AAHp-XMrbGqnuTdad1Hw4WbhnePDaWKI04s/getUpdates
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Visible = false;
+
+                StartSession = DateTime.Now;
+
+                Stream data = webClient.OpenRead("https://2ip.ru/");
+                StreamReader reader = new StreamReader(data);
+                Regex regex = new Regex(@"([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})");
+                MatchCollection match = regex.Matches(reader.ReadToEnd());
+
+                SendMessage($" Мой Господин, ваш компьютер только что был запущен.\n " +
+                    $"Ваш IP:  {match[0].Value}\n " +
+                    $"Напоминаю:  3389\n" +
+                    $"    Хорошего дня  =)\n\n" +
+                    $"Сегодня:   {DateTime.Now.ToLongDateString()}");
+
+
+                notifyIcon1.BalloonTipText = $"Ваш IP:  {match[0].Value}";
+                notifyIcon1.ShowBalloonTip(1000);
+
+                timer1.Start();
+
+                //Application.Exit();
+
+                response = webClient.DownloadString("https://api.telegram.org/bot" + Token + "/getUpdates?offset=" + (LastUpdateID));
+                if (response.Length > 23)
+                {
+                    var N = JSON.Parse(response);
+                    foreach (JSONNode r in N["result"].AsArray)
+                    {
+                        LastUpdateID = r["update_id"].AsInt + 1;
+                        string UserMess = r["message"]["text"];
+                    }
+                }
+            }
+            catch(Exception er)
+            {
+                MessageBox.Show(er.Message);
+            }
+        }
+        #region EndApp
+        private void Form1_VisibleChanged(object sender, EventArgs e)
+        {
+            if (flag == false)
+                Visible = false;
+            richTextBox1.Clear();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            TimeSpan span = new TimeSpan();
+            span = DateTime.Now - StartSession;
+            string str;
+
+            if (span.Minutes <= 1)
+                str = $"{span.Seconds} c.";
+            else if (span.Hours <= 1)
+                str = $"{span.Minutes} м. {span.Seconds} c.";
+            else if (span.Days <= 1)
+                str = $"{span.Hours} ч. {span.Minutes} м. {span.Seconds} c.";
+            else
+                str = $"{span.Days} д. {span.Hours} ч. {span.Minutes} м. {span.Seconds} c.";
+
+
+            webClient.OpenRead($"https://api.telegram.org/bot541751696:AAHp-XMrbGqnuTdad1Hw4WbhnePDaWKI04s/sendMessage?chat_id=349870760&text=" +
+                $" Мой Господин, ваш компьютер только что был выключен.\n " +
+                $"Ваш компьютер был в сети:   {str}");
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            flag = true;
+            Visible = true;
+            SendMessage("Чат включен.");
+        }
+        #endregion
+
+        int LastUpdateID = 0;
+        string response;
+
+        public static void HttpUploadFile(string url, string file, string paramName, string contentType, NameValueCollection nvc)
+        {
+            Console.WriteLine(string.Format("Uploading {0} to {1}", file, url));
+            string boundary = "---------------------------" + DateTime.Now.Ticks.ToString("x");
+            byte[] boundarybytes = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "\r\n");
+
+            HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(url);
+            wr.ContentType = "multipart/form-data; boundary=" + boundary;
+            wr.Method = "POST";
+            wr.KeepAlive = true;
+            wr.Credentials = System.Net.CredentialCache.DefaultCredentials;
+
+            Stream rs = wr.GetRequestStream();
+
+            string formdataTemplate = "Content-Disposition: form-data; name=\"{0}\"\r\n\r\n{1}";
+            foreach (string key in nvc.Keys)
+            {
+                rs.Write(boundarybytes, 0, boundarybytes.Length);
+                string formitem = string.Format(formdataTemplate, key, nvc[key]);
+                byte[] formitembytes = System.Text.Encoding.UTF8.GetBytes(formitem);
+                rs.Write(formitembytes, 0, formitembytes.Length);
+            }
+            rs.Write(boundarybytes, 0, boundarybytes.Length);
+
+            string headerTemplate = "Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\nContent-Type: {2}\r\n\r\n";
+            string header = string.Format(headerTemplate, paramName, file, contentType);
+            byte[] headerbytes = System.Text.Encoding.UTF8.GetBytes(header);
+            rs.Write(headerbytes, 0, headerbytes.Length);
+
+            FileStream fileStream = new FileStream(file, FileMode.Open, FileAccess.Read);
+            byte[] buffer = new byte[4096];
+            int bytesRead = 0;
+            while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                rs.Write(buffer, 0, bytesRead);
+            }
+            fileStream.Close();
+
+            byte[] trailer = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "--\r\n");
+            rs.Write(trailer, 0, trailer.Length);
+            rs.Close();
+
+            WebResponse wresp = null;
+            try
+            {
+                wresp = wr.GetResponse();
+                Stream stream2 = wresp.GetResponseStream();
+                StreamReader reader2 = new StreamReader(stream2);
+                Console.WriteLine(string.Format("File uploaded, server response is: {0}", reader2.ReadToEnd()));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error uploading file", ex);
+                if (wresp != null)
+                {
+                    wresp.Close();
+                    wresp = null;
+                }
+            }
+            finally
+            {
+                wr = null;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            response = webClient.DownloadString("https://api.telegram.org/bot" + Token + "/getUpdates?offset=" + (LastUpdateID));
+            if (response.Length > 23)
+            {
+                var N = JSON.Parse(response);
+                foreach (JSONNode r in N["result"].AsArray)
+                {
+                    LastUpdateID = r["update_id"].AsInt + 1;
+                    string UserMess = r["message"]["text"];
+                    int ID = r["message"]["from"]["id"].AsInt;
+
+                    if (ID == 349870760)
+                    {
+                        if (UserMess == "/chatoff")
+                        {
+                            flag = false;
+                            Visible = false;
+                            SendMessage("Чат выключен.");
+                        }
+                        else if (UserMess == "/mute")
+                        {
+                            Mute();
+                        }
+                        else if (UserMess == "/screen")
+                        {
+                            Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+
+                            Graphics graphics = Graphics.FromImage(printscreen as Image);
+
+                            graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
+
+                            printscreen.Save(@"D:\ip\Screen\printscreen.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                            NameValueCollection nvc = new NameValueCollection();
+                            nvc.Add("chat_id", ID.ToString());
+                            
+                            HttpUploadFile("https://api.telegram.org/bot" + Token + "/sendPhoto",
+                                 @"D:\ip\Screen\printscreen.jpg", "photo", "image/jpeg", nvc);
+
+                        }
+                        else if (UserMess == "/volup")
+                        {
+                            VolUp();
+                        }
+                        else if (UserMess == "/voldown")
+                        {
+                            VolDown();
+                        }
+                        else if (UserMess == "/off")
+                        {
+                            System.Diagnostics.Process process = new System.Diagnostics.Process();
+                            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                            startInfo.FileName = "powershell.exe";
+                            startInfo.Arguments = "shutdown -s -t 00";
+                            process.StartInfo = startInfo;
+                            process.Start();
+                        }
+                        else if (UserMess == "/restart")
+                        {
+                            System.Diagnostics.Process process = new System.Diagnostics.Process();
+                            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                            startInfo.FileName = "powershell.exe";
+                            startInfo.Arguments = "shutdown -r -t 00";
+                            process.StartInfo = startInfo;
+                            process.Start();
+                        }
+                        else if (UserMess == "/chat")
+                        {
+                            flag = true;
+                            Visible = true;
+                            SendMessage("Чат включен.");
+                            richTextBox2.Focus();
+                        }
+                        else if (Visible == true)
+                        {
+                            richTextBox1.Text += "Client:\n\t" + UserMess + "\n";
+                        }
+                        else if (UserMess == "hi" || UserMess == "Hi" || UserMess == "Hello" && flag == false)
+                            SendMessage("Hi");
+                        else
+                        {
+                            MessageBox.Show(UserMess);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SendMessage(richTextBox2.Text);
+            richTextBox1.Text += "User:\n\t" + richTextBox2.Text + "\n";
+            richTextBox2.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            flag = false;
+            Visible = false;
+            SendMessage("Чат выключен.");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            flag = false;
+            Visible = false;
+            notifyIcon1.BalloonTipText = $"Окно свернуто.\nНажмите на иконку два раза чтобы розвернуть.";
+            notifyIcon1.ShowBalloonTip(1000);
+            HideFlag = true;
+        }
+
+        bool HideFlag = false;
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (HideFlag == true)
+            {
+                flag = true;
+                Visible = true;
+                HideFlag = false;
+            }
+        }
+
+        private Point mouseOffset;
+        private bool isMouseDown = false;
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            int xOffset;
+            int yOffset;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                xOffset = -e.X - SystemInformation.FrameBorderSize.Width;
+                yOffset = -e.Y - SystemInformation.CaptionHeight -
+                    SystemInformation.FrameBorderSize.Height;
+                mouseOffset = new Point(xOffset, yOffset);
+                isMouseDown = true;
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown)
+            {
+                Point mousePos = Control.MousePosition;
+                mousePos.Offset(mouseOffset.X, mouseOffset.Y);
+                Location = mousePos;
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isMouseDown = false;
+            }
+        }
+    }
+}
